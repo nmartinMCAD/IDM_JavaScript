d42352e5a2700d2383ab4f3c7aeebf45a797b5f6
nmartinMCAD <nmartin@mcad.edu>
2017-11-02 11:22:34 -0600

    Exercise 10_1 Final Commit

---
+++ wk_10/exercise_10-1/HPlusSport_10-1/script.js
@@ -0,0 +1,5 @@
+$(function(){
+      $("h2[data-type=vitamin]").css("background-color", "#3D9970");
+      $("h2[data-type=mineralwater]").css("background-color", "#0074D9");
+      $("h2[data-type=proteinbar]").css("background-color", "#85144b");
+});
